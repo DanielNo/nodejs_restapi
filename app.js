1dce73c04e1deea3c0b986ba8bea43c782cfad3c
Daniel No <mr.danielno@gmail.com>
2020-10-12 17:07:17 -0400

    setup app.js, install express, npm init package json, nodemon

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+app.get("/",(req,res) =>{
+   console.log("responding to root route")
+   res.send("hello from root!") 
+})
+
+app.get("/users",(req,res) =>{
+    var user1 = {firstName: "Stephen", lastName: "Curry"}
+    var user2 = {firstName: "Kevin", lastName: "Durant"}
+    res.json([user1,user2])
+
+ })
+ 
+
+app.listen(3003,() => {
+console.log("Server is up and listening on port 3003...")
+})
